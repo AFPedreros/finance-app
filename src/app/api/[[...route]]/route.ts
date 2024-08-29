1dbcd81dbff2e5af02655345e4f86bba53dfa33c
afpedreros <felipe.pedreros94@gmail.com>
2024-08-29 09:27:47 -0500

    chore: Update API endpoint in getHello function

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,23 +1,10 @@
 import { Hono } from "hono";
-import { logger } from "hono/logger";
 import { handle } from "hono/vercel";
-
-export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.use("*", logger());
-
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
-
-app.all("*", (c) => c.text("404: Not Found"));
+app.get("/hello", (c) => c.json("Hello, Vercel!"));
+app.get("/hello/:name", (c) => c.text(`Hello, ${c.req.param("name")}!`));
 
 export const GET = handle(app);
 export const POST = handle(app);
-export const PUT = handle(app);
-export const PATCH = handle(app);
-export const DELETE = handle(app);
